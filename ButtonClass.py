4d855c87b613568bdf88cf5342976139f43e9135
name <jamarict04@gmail.com>
2022-11-27 22:00:59 -0500

    delete old animations + basic unit class

--- ButtonClass.py
+++ ButtonClass.py
@@ -93,7 +93,7 @@ class ParameterRectButton(RectangleButton): # Converting to Map Metrics
 
 #Button Functions
 
-def backToMain(app): # Reset going back to Title
+def goToSetup(app): # Reset going back to Title
     app.playerNum = 0
     app.mapSize = 0
     app.mapText = "No"
--- UnitClass.py
+++ UnitClass.py
@@ -7,33 +7,14 @@ class Unit(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.cost = 2
-        self.health = 10     
-        self.attack = 2
-        self.defense = 2
-        self.movement = 1
-        self.range = 1
         self.color = "red"
-        self.canMove = True
-        self.canAttack = True
-        self.canAct = True
 
     def __repr__(self):
         return f"Warrior({self.x, self.y}"
+    
+
         
     #units redraw themselves based on getCellBounds outputs
     def redraw (self, app, canvas, x0, y0, x1, y1):
         r = 5
-        canvas.create_oval(x0+r, y0+r, x1-r, y1-r, fill = self.color)
-    
-    # Legailty check of canMove. Units can move if the location is within their
-    # movement ranges
-    def moveUnit(self, newX, newY):
-        if self.canMove == True:
-            if ((newX > self.x + self.movement) or (newX < self.x - self.movement) 
-                or (newY > self.y + self.movement) or (newY < self.y - self.movement)):
-                    pass
-            else:
-                self.x = newX
-                self.y = newY
-                self.canMove = False
+        canvas.create_oval(x0 + r, y0 + r, x1 -r, y1 - r, fill = self.color, width = 5)
